741a3da36a5c956109db03ae73f03199302aad78
amirh0ssein-AF <4444.amirhossein.4444@gmail.com>
2026-02-12 15:19:18 +0330

    Adding the DbContext and its required packages for database connectivity

---
+++ StockMarket/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockMarket.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
